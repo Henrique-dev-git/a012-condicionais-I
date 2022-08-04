cbf59d284f50be154369c5840a83b2829a933d25
Henrique-dev-git <carlos.henrique.psic@gmail.com>
2022-08-04 18:23:06 -0300

    exercícios prontos

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,21 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = 18;
+let terminouEnsinoMedio = true;
+let graduação = false;
 
-if(){
-
+if(idade <= 18){
+    console.log('é maior de idade')
 }else{
-	
+    console.log('menor de idade')
+}	
+
+if(terminouEnsinoMedio){
+    console.log('médio completo')
+}else{
+    console.log('médio incompleto')
+}	
+
+if(graduação){
+    console.log('nível superior')
+}else{
+    console.log('não graduado')	
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,15 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt("Escreva sua nacionalidade aqui!");
+
+if (nacionalidade === "brasileira"){
+    console.log("Sua nacionalidade é brasileira!")
+}else if(nacionalidade === "argentina"){
+    console.log('Sua nacionalidade é argentina!');
+}else if(nacionalidade === "uruguaia"){
+    console.log('Sua nacionalidade é uruguaia!');
+}else if(nacionalidade === "chilena"){
+    console.log('Sua nacionalidade é chilena!');
+}else if(nacionalidade === "colombiana"){
+    console.log('Sua nacionalidade é colombiana!');
+}else{
+    console.log('Nacionalidade não corresponde a nenhum dos valores!')
+}
